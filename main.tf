a80cb2e8f248644fa518b5e67536387837bfa728
sc-rpt <52003639+sc-rpt@users.noreply.github.com>
2021-01-28 09:17:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,3 +24,39 @@ module "networking" {
   vnet_address_spacing    = var.vnet_address_spacing
   subnet_address_prefixes = var.subnet_address_prefixes
 }
+
+  module "webserver" {
+  source  = "app.terraform.io/YOUR_ORG_NAME/webserver/azurerm"
+  version = "0.12.0"
+
+  name      = var.name
+  location  = var.location
+  subnet_id = module.networking.subnet-ids[0]
+  vm_count  = 1
+  username  = var.username
+  password  = var.password
+}
+
+module "appserver" {
+  source  = "tap-tfe.digitalinnovation.dev/YOUR_ORG_NAME/appserver/azurerm"
+  version = "0.12.0"
+
+  name      = var.name
+  location  = var.location
+  subnet_id = module.networking.subnet-ids[1]
+  vm_count  = 1
+  username  = var.username
+  password  = var.password
+}
+
+module "dataserver" {
+  source  = "tap-tfe.digitalinnovation.dev/YOUR_ORG_NAME/dataserver/azurerm"
+  version = "0.12.0"
+
+  name      = var.name
+  location  = var.location
+  subnet_id = module.networking.subnet-ids[2]
+  vm_count  = 1
+  username  = var.username
+  password  = var.password
+}
